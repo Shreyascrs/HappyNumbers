b44ac41a8540e00d7040adc8f63ebab8be704e8e
shreyascr <shrs601@gmail.com>
2019-10-02 08:30:22 +0530

    Refactor. static imports and other setup

--- src/test/java/com/thoughtworks/TestHappyNumber.java
+++ src/test/java/com/thoughtworks/TestHappyNumber.java
@@ -1,21 +1,32 @@
 package com.thoughtworks;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestHappyNumber {
 
+    HappyNumber happyNumber;
+
+    @BeforeEach
+    void setup() {
+        happyNumber = new HappyNumber();
+    }
+
     @Test
     void GivenNumberZero_WhenCheck_MustBeChecked() {
-        HappyNumber happyNumber = new HappyNumber();
+
         int number = 0;
-        Assertions.assertEquals(false, happyNumber.check(number));
+
+        assertEquals(false, happyNumber.check(number));
     }
 
     @Test
     void GivenNumberOne_WhenCheck_MustBeChecked() {
-        HappyNumber happyNumber = new HappyNumber();
+
         int number = 1;
-        Assertions.assertEquals(false, happyNumber.check(number));
+
+        assertEquals(false, happyNumber.check(number));
     }
 }
