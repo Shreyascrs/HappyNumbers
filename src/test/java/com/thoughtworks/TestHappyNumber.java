3e43c35dad938d82ce16753ea9867680092881d3
shreyascr <shrs601@gmail.com>
2019-10-02 08:27:06 +0530

    Add. given one must not be a HappyNumber

--- src/test/java/com/thoughtworks/TestHappyNumber.java
+++ src/test/java/com/thoughtworks/TestHappyNumber.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 public class TestHappyNumber {
 
     @Test
-    void GivenNumber_WhenCheck_MustBeChecked() {
+    void GivenNumberZero_WhenCheck_MustBeChecked() {
         HappyNumber happyNumber = new HappyNumber();
         int number = 0;
+        Assertions.assertEquals(false, happyNumber.check(number));
+    }
+
+    @Test
+    void GivenNumberOne_WhenCheck_MustBeChecked() {
+        HappyNumber happyNumber = new HappyNumber();
+        int number = 1;
         Assertions.assertEquals(false, happyNumber.check(number));
     }
 }
